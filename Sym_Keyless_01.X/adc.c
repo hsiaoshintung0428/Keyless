15010af6cfc2f8081ad494e441e30bef9873e262
hsiaoshintung0428 <hsiao0428@gmail.com>
2024-01-13 23:20:13 +0800

    adder adc call function ...

--- Sym_Keyless_01.X/adc.c
+++ Sym_Keyless_01.X/adc.c
@@ -7,38 +7,39 @@
 
 #define _Internal_ADC_ON(n)         (ADCON0bits.ADON = n )
 #define _Internal_ADC_GO(n)         (ADCON0bits.GO  = n)
-#define _Internal_ADC_DONE()        (ADCON0bits.GO )
+#define _Internal_ADC_DONE()        (!ADCON0bits.GO )
 #define _INternal_ADC_Channel(n)    (ADCON0bits.CHS = n)   
-typedef enum{
-    Channel_AN0 =0  ,
-    Channel_AN1     ,
-    Channel_AN2     ,  
-    Channel_AN3     ,  
-    Channel_AN4     ,
-    Channel_AN5     ,  
-    Channel_AN6     ,
-    Channel_AN7     ,  
-    Channel_AN8     ,
-    Channel_AN9     ,
-    Channel_AN10     ,  
-    Channel_AN11     ,  
-    Channel_AN12     ,
-    Channel_AN13     ,  
-    Channel_AN14     ,
-    Channel_AN15     ,  
-    Channel_AN16     ,
-    Channel_AN17     ,
-    Channel_AN18     ,  
-    Channel_AN19     ,  
-    Channel_AN20     ,
-    Channel_AN21     ,  
-    Channel_AN22     ,
-    Channel_AN23     ,  
-    Channel_AN24     , 
-    Channel_AN25     ,
-    Channel_AN26     ,  
-    Channel_AN27     ,              
-    }PIC_ADC_Channel;
+
+//typedef enum{
+//    Channel_AN0 =0  ,
+//    Channel_AN1     ,
+//    Channel_AN2     ,  
+//    Channel_AN3     ,  
+//    Channel_AN4     ,
+//    Channel_AN5     ,  
+//    Channel_AN6     ,
+//    Channel_AN7     ,  
+//    Channel_AN8     ,
+//    Channel_AN9     ,
+//    Channel_AN10     ,  
+//    Channel_AN11     ,  
+//    Channel_AN12     ,
+//    Channel_AN13     ,  
+//    Channel_AN14     ,
+//    Channel_AN15     ,  
+//    Channel_AN16     ,
+//    Channel_AN17     ,
+//    Channel_AN18     ,  
+//    Channel_AN19     ,  
+//    Channel_AN20     ,
+//    Channel_AN21     ,  
+//    Channel_AN22     ,
+//    Channel_AN23     ,  
+//    Channel_AN24     , 
+//    Channel_AN25     ,
+//    Channel_AN26     ,  
+//    Channel_AN27     ,              
+//    }PIC_ADC_Channel;
 
 #define ADC_Right_justified()   ADCON1bits.ADFM=1   //ADC result formal right
 #define ADC_Left_justified()    ADCON1bits.ADFM=0
@@ -56,6 +57,8 @@ typedef enum{
 #define ADC_pREF_InREF()        ADCON1bits.ADPREF=3   
     
     
+ void Trigger_ADC(uint8_t channel);
+ uint16_t Get_ADC_Result(void);
     
 void Init_ADC(void)
 {
@@ -64,24 +67,49 @@ void Init_ADC(void)
     ADC_nREF_VSS();
     ADC_pREF_VDD();
     ADCON2  = 0; //Disable Auto trigger 
+    _Internal_ADC_ON(ENABLE);//Enable ADC module ~~ 
     
 }
 
 
 void Trigger_ADC(uint8_t channel)
 {
+    ADCON0 = channel ;
     _Internal_ADC_ON(ENABLE);
     
 }
 
 uint16_t Get_ADC_Result(void)
 {
-    uint16_t ADC_Temp;
+     union{
+     struct {
+                uint8_t LSB :8;//bit0-bit3
+                uint8_t MSB :8;//bit4-bit7
+        }byte;
+        uint16_t All;
+    }_ADCResult;
     
+      while(_Internal_ADC_DONE());//wait ADC converter done...
+      
     _Internal_ADC_GO(0);
-    ADC_Temp = ADRESH;
-    ADC_Temp = (ADC_Temp <<8 )+ ADRESL;
+    _ADCResult.byte.MSB = ADRESH ;
+    _ADCResult.byte.LSB = ADRESL ;
+
     
-    return ADC_Temp;
+    return _ADCResult.All;
             
 }
+
+uint16_t Internal_ADC_Conversion (uint8_t channel, uint8_t Active )
+{
+    uint16_t date =0;
+    
+    if(Active =ENABLE)
+    {
+        Trigger_ADC(channel);
+        date    =   Get_ADC_Result();
+    }
+            
+    return date;
+}
+  
---
+++ Sym_Keyless_01.X/adc.h
@@ -0,0 +1,57 @@
+/* 
+ * File:   adc.h
+ * Author: Varcolac_MV_SKU36
+ *
+ * Created on 2024年1月13日, 下午 11:03
+ */
+
+#ifndef ADC_H
+#define	ADC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+typedef enum{
+    Channel_AN0 =0  ,
+    Channel_AN1     ,
+    Channel_AN2     ,  
+    Channel_AN3     ,  
+    Channel_AN4     ,
+    Channel_AN5     ,  
+    Channel_AN6     ,
+    Channel_AN7     ,  
+    Channel_AN8     ,
+    Channel_AN9     ,
+    Channel_AN10     ,  
+    Channel_AN11     ,  
+    Channel_AN12     ,
+    Channel_AN13     ,  
+    Channel_AN14     ,
+    Channel_AN15     ,  
+    Channel_AN16     ,
+    Channel_AN17     ,
+    Channel_AN18     ,  
+    Channel_AN19     ,  
+    Channel_AN20     ,
+    Channel_AN21     ,  
+    Channel_AN22     ,
+    Channel_AN23     ,  
+    Channel_AN24     , 
+    Channel_AN25     ,
+    Channel_AN26     ,  
+    Channel_AN27     ,              
+    }PIC_ADC_Channel;
+
+extern void Init_ADC(void);
+extern int16_t Internal_ADC_Conversion (uint8_t channel, uint8_t Active );
+
+#endif	/* ADC_H */
+
