08ff7ce8313f631028debe3b24cc54f11362ff0b
hsiaoshintung0428 <hsiao0428@gmail.com>
2024-01-13 23:19:35 +0800

    Added Timer1 module setting...

--- Sym_Keyless_01.X/TMR.c
+++ Sym_Keyless_01.X/TMR.c
@@ -12,12 +12,24 @@
 #include "global.h"
 #include "TMR.h"
 
+
+
+
+void Init_TMR1(void);
+void Set_TMR1(uint16_t value);
+
 //
+void Init_TMR(void)
+{
+    Init_TMR1();
+}
+
 void Init_TMR0(void)
 {
-    OPTION_REGbits.PS   = Timer0PS;
-    OPTION_REGbits.PSA = ENABLE;
-    OPTION_REGbits.T0CS = 0;
+//    OPTION_REGbits.PS   = Timer0PS;
+//    OPTION_REGbits.PSA = ENABLE;
+//    OPTION_REGbits.T0CS = 0;
+
 }
 //
 void RestoreTMR0(uint8_t value)
@@ -26,12 +38,54 @@ void RestoreTMR0(uint8_t value)
  
 }
 
+void Init_TMR1(void)
+{
+    Set_TMR1Source(TMR1_Source_CoreOSC);
+    Set_TMR1Externalcircuit(DISABLE);
+    Set_TMR1ScaleOSC(TMR1Rate8);
+    Set_TMR1SYNCFOSC(ENABLE);
+    T1GCON =0;
+}
+
+void Set_TMR1(uint16_t value)
+{
+    union{
+     struct {
+                uint8_t LSB :8;//bit0-bit3
+                uint8_t MSB :8;//bit4-bit7
+        }byte;
+        uint16_t All;
+    }_TMR1Count;
+    
+    _TMR1Count.All = value;
+    Set_TMR1ON(DISABLE);
+    TMR1H = _TMR1Count.byte.MSB;
+    TMR1L = _TMR1Count.byte.LSB;
+}
+
+void Run_TMR1(uint8_t value ,uint16_t count)
+{
+    Set_TMR1(count);
+    Set_TMR1ON(value);
+}
+
+
+
+
 void Init_TMR2(void)
+{
+    T2CLKCONbits.CS = 0x01; //FSYS
+    T2CONbits.ON =0;
+    T2CONbits.CKPS = 0x07;//1:128
+    T2CONbits.OUTPS =0x11;//1:10 
+}
+
+void SetTMR2(uint8_t value)
 {
     
 }
 
-void SetTMR2(uint8_t value)
+void Run_TMR2(uint8_t value ,uint8_t count)
 {
     
 }
--- Sym_Keyless_01.X/TMR.h
+++ Sym_Keyless_01.X/TMR.h
@@ -24,11 +24,16 @@ typedef enum{
 }_TMR0Rate; 
 
 
-#define PollingTime 5 //mS 
+#define PollingTime 10 //mS 
 #define PollingTimeToFeq    (1000uL/PollingTime)
-#define Timer0PS    TMR0Rate256
-#define Timer0Rate  (Timer0PS+1)
-#define FillTMR0    ((uint16_t) 256-(((FSYS/PollingTimeToFeq)<<Timer0Rate)))
+#define TMR1PS      TMR1Rate8
+#define FillTMR1    (uint16_t) (0xFFFF-((FSYS>>TMR1Rate8)/PollingTimeToFeq))
+
+
+
+//#define Timer0PS    TMR0Rate256
+//#define Timer0Rate  (Timer0PS+1)
+//#define FillTMR0    ((uint16_t) 256-(((FSYS/PollingTimeToFeq)<<Timer0Rate)))
 //#define FillTMR0    (256-157)
 //OPTION_REG = 0x87;
 //TMR0 = 256- FillTMR0;
@@ -36,12 +41,43 @@ typedef enum{
 // 
 //CPS	 = (8000000/200)/256 =157  
 
+typedef enum{
+    TMR1Rate1 = 0,  //1:1
+    TMR1Rate2 = 1,  //1:2    
+    TMR1Rate4= 2,   //1:4  
+    TMR1Rate8 =3,   //1:8  
+}_TMR1CKPS; 
+
+typedef enum{
+    TMR1_Source_InterLF = 0,//internal Low power 31kHz 
+    TMR1_Source_ExterLF  = 1,//External Low power 32kHz   
+    TMR1_Source_SysOSC= 2,  //FOSC
+    TMR1_Source_CoreOSC =3, //FOSC/4  
+}_TMR1Source; 
+
+//--- TMR0 ----//
+#define Set_TMR1Source(n)   T1CONbits.CS =n 
+#define Set_TMR1ScaleOSC(n) T1CONbits.CKPS =n
+#define Set_TMR1Externalcircuit(n)  T1CONbits.OSCEN =n
+#define Set_TMR1ON(n)   T1CONbits.ON = n
+#define Set_TMR1SYNCFOSC(n) T1CONbits.SYNC =n
+
+#define Set_TMR1LPOSC(n)    T1CONbits.OSCEN =n
 
 
+
+
+
+
+
+extern void Init_TMR(void);
 extern void Init_TMR0(void);
 extern void RestoreTMR0(uint8_t value);
 extern void Init_TMR2(void);
 
+//---extern API---//
+extern void Run_TMR1(uint8_t value ,uint16_t count);
+extern void Run_TMR2(uint8_t value ,uint8_t count);
 
 #ifdef	__cplusplus
 }
