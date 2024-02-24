f6742bf4c715f6e5cc7d418a025e20921dc7baeb
hsiaoshintung0428 <hsiao0428@gmail.com>
2024-02-24 20:54:00 +0800

    新增方向燈控制功能

--- Sym_Keyless_01.X/process.c
+++ Sym_Keyless_01.X/process.c
@@ -11,9 +11,13 @@
 #include "global.h"
 #include "ioports.h"
 #include "TMR.h"
+#include "delay.h"
 #include "process.h"
 
 uint8_t PowerState = 0xFF;
+uint16_t  Timestamp =0;
+
+
 
 //DRV8872
 void DRV8872_WakeUp(void);
@@ -26,45 +30,67 @@ void DRV8872_Brake(void);
 
 //龍頭解鎖
 void Unlock_Handle(void)
-{
+{   
+    stuTHCML *th;
+    th->date.dt.day =1;
+    
     DRV8872_WakeUp();
-    
+    _delay1uS(50);
     DRV8872_Forward();
-    
+    _delay1mS(40);
 }
 
 //龍頭上鎖
 void Lock_Handle(void)
 {
     DRV8872_WakeUp();
-    
+    _delay1uS(50);
     DRV8872_Reverse();
-    
+     _delay1mS(40);
     DRV8872_Sleep();
 }
 
-//閃爍方向燈
-void Blinking_TurnSignal(uint8_t control )
+
+void Set_TurnSignal(uint8_t Dir, uint8_t control )
 {
-    
+    switch (Dir)
+    {
+        case DirectionRight:
+            SetLAMP_Right(control);
+            break;
+            
+        case DirectionLeft:
+            SetLAMP_Left(control);
+            break;
+            
+        case DirectionAll:
+            SetLAMP_Right(control);
+            SetLAMP_Left(control);
+            break;
+    }
 }
 
 void DRV8872_WakeUp(void)
 {
     //wark up DRV8872 first 
     //Set PWM_IN1& 2 to still high level more than 5 us
+    //device is operational 50 µs (tON) later 
     //[7.3.2 Sleep mode]  
     SetMOTOPWM1(ENABLE);
     SetMOTOPWM1(ENABLE);
+    _delay1uS(5);
+    
+    
 }
 
 void DRV8872_Sleep(void)
 {
     //wark up DRV8872 first 
-    //Set PWM_IN1& 2 to still high level more than 1 ms
+    //Set PWM_IN1& 2 to still low level more than 1 ms
     //[7.3.2 Sleep mode]  
-    SetMOTOPWM1(ENABLE);
-    SetMOTOPWM1(ENABLE);
+    SetMOTOPWM1(DISABLE);
+    SetMOTOPWM1(DISABLE);
+    _delay1mS(1);
 }
 
 void DRV8872_Reverse(void)
--- Sym_Keyless_01.X/process.h
+++ Sym_Keyless_01.X/process.h
@@ -27,7 +27,41 @@ typedef enum{
     PS_G3       = 0xFF //global reset        
 }_MCUPowerState;
 
+typedef enum{
+    DirectionRight =0,
+    DirectionLeft =1,
+    DirectionAll  =2,
+}_DirectionLamp;
 extern uint8_t PowerState;
 
+typedef enum { ndtYear, ndtMonth, ndtDay, ndtHour, ndtMinute, ndtSecond, MAXndtDATE } seqDATE;
+typedef union  {
+                          U8 n[MAXndtDATE];
+                          struct {
+                                    U8 year;   // 0..99 for 2000..2099
+                                    U8 month;  // 1..12
+                                    U8 day;    // 1..31
+                                    U8 hour;   // 0..23
+                                    U8 minute; // 0..59
+                                    U8 second; // 0..59
+                                 } dt;
+                       } stuDATE  ,mDate;       
+typedef enum { ntkTEMP, ntkRH, ntkCO2, MAXntkTHCMLKIND } seqTHCMLKIND;                       
+typedef union  {
+                          S16 va[MAXntkTHCMLKIND];
+                          struct {
+                                    S16 TEMP;
+                                    U16 RH;
+                                    U16 CO2;
+                                 } kd;
+                       } stuValueTHCML;
+typedef struct {
+                         stuValueTHCML value;
+                               stuDATE date;
+                       } stuTHCML;
+
+extern void Unlock_Handle(void);
+extern void Lock_Handle(void);
+extern void Set_TurnSignal(uint8_t Dir, uint8_t control);
 #endif	/* PROCESS_H */
 
