413e8d0fa8aa3ae06add84d5dbdbf48d2749f4e6
hsiaoshintung0428 <hsiao0428@gmail.com>
2024-04-21 23:47:39 +0800

    correct spi function (maybe

--- Sym_Keyless_01.X/spi.c
+++ Sym_Keyless_01.X/spi.c
@@ -1,9 +1,13 @@
 
+
 /* 
  * File:   spi.c
- * Author: Norton.Hsioa
+ * Author: 
  *
  * Created on 
+ * SPI mode Mode 3?
+ * 修改SPI
+ * 
  */
 
 #include <stdio.h>
@@ -17,25 +21,45 @@
 
 void Init_SPI(void)
 {
-
-    
+    SSP1ADD = 1 ;
     // this setting selects master mode with frequency fosc/64
-    SSP1CON1bits.SSPM0 = 1;
-    SSP1CON1bits.SSPM1 = 0;
-    SSP1CON1bits.SSPM2 = 0;
-    SSP1CON1bits.SSPM3 = 0;
+    //32MHz/64=500KHz
+    SSP1CON1bits.SSPM = 0b0010;
+    
+    
+    //32MHz/(4*(SSP1ADD+1))=250KHz
+//    SSP1ADD = 31 ;
+//    SSP1CON1bits.SSPM=0b1010;
     // Enable SPI Port
+    
     SSP1CON1bits.SSPEN = 1;
     // Configure The Clock Polarity & Phase
+    // SPI waveform 
     SSP1CON1bits.CKP = 0;
     
-    SSPSTATbits.CKE = 0;
+    SSP1STATbits.CKE = 1;
+    SSP1STATbits.SMP = 1;
+
+    SSP1CON1bits.WCOL = 0;
+    
+    LATDbits.LATD3 = 1;
+    TRISDbits.TRISD3 = 0; // CS  (ATA5781 PB5) -> Output(RD3)
+    
+    
     //  Slew rate control enabled for High Speed mode
     SSPSTATbits.SMP = 0;
    // Set SPI pins as digital I/O
-    TRISC5 = 0; // SDO -> Output
-    TRISC4 = 1; // SDI -> Input
-    TRISC3 = 0; // SCK -> Output
+    TRISCbits.TRISC3 = 0; // CLK (ATA5781 PB1) -> Output
+    LATCbits.LATC3 = 1;
+    
+    TRISCbits.TRISC5 = 0; // MOSI(ATA5781 PB2) -> Output
+    LATCbits.LATC5 = 1;
+    
+    TRISCbits.TRISC4 = 1; // MISO(ATA5781 PB3) -> Input
+    LATCbits.LATC4 = 0;
+  
+
+
     //Uncommnent this line, if you want to send send over interrupt
     // SSPIE = 1; PEIE = 1; GIE = 1;
 }
@@ -44,15 +68,22 @@ void Init_SPI(void)
 unsigned char at_rx_spi(unsigned char data)
 {
     uint8_t count = 0;
+    
+    if(SSP1CON1bits.SSPOV)
+    {
+        count = SSP1BUF ;
+    }
+    
     SSP1BUF = data;
-   
+    
     while(SSP1STATbits.BF)
     {
-        count++;
-       _delay1uS(125);
     }
-    return SSP1BUF;
+     _delay1uS(50);
+  
+     return SSP1BUF;
 }
+
 
 void at_tx_spi(unsigned char data)
 {
@@ -61,8 +92,6 @@ void at_tx_spi(unsigned char data)
    
     while(SSP1STATbits.BF)
     {
-        count++;
-       _delay1uS(125);
     }
 }
 
@@ -83,4 +112,22 @@ void at_writespi(unsigned char *DataPtr, unsigned char length)
 		at_tx_spi(*DataPtr);
 		DataPtr++;
         }   
+}
+
+
+void rf_reset_spi(void)
+{
+    //Config SPI Lines and handshake lines for ATA5291 & ATtiny261
+//	SPI_SCK_IO_OUT	= GPIO_SET_AS_PULL_UP; // set pull-up on PB1 (SCLK)
+//	SPI_MOSI_IO_OUT	= GPIO_SET_AS_PULL_UP; // set pull-up on PB2 (MOSI)
+//	SPI_MISO_IO_OUT	= GPIO_SET_AS_PULL_UP; // set pull-up on PB3 (MISO)
+//
+//	RF_IRQ_IO_OUT	= GPIO_SET_AS_PULL_UP; //PORTE |=  ( 1 << 4 ); // set pull-up on PE4 (IRQ  -> IRQ1)
+//
+//	SPI_SCK_IO_DIR	= GPIO_SET_AS_INPUT;  //SCLK = PB1 input
+//	SPI_MOSI_IO_DIR	= GPIO_SET_AS_INPUT;  //MOSI = PB2 input
+//	SPI_MISO_IO_DIR	= GPIO_SET_AS_INPUT;  //MISO = PB3 input
+//
+//	RF_IRQ_IO_DIR	= GPIO_SET_AS_INPUT; //DDRE &= ~( 1 << 4 );  //IRQ  = PE4 input
+
 }
--- Sym_Keyless_01.X/spi.h
+++ Sym_Keyless_01.X/spi.h
@@ -13,12 +13,16 @@ extern "C" {
 #endif
 
 extern void Init_SPI(void);
+extern void rf_reset_spi(void);
+
 extern unsigned char at_rx_spi(unsigned char data);
 
 extern void at_tx_spi(unsigned char data);
 extern void at_readspi(unsigned char *DataPtr,unsigned char length, unsigned char out_byte);
 extern void at_writespi(unsigned char *DataPtr, unsigned char length);
 
+
+//#define rf_set_spi() Init_SPI()
 #ifdef	__cplusplus
 }
 #endif
